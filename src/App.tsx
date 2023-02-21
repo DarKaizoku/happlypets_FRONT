f418691967cfef39a037f76e15604660d3297e47
WAM <eraserw@gmail.com>
2023-02-21 16:52:52 +0100

    try token

--- src/App.tsx
+++ src/App.tsx
@@ -9,36 +9,54 @@ const baseUrl = 'http://localhost:3000/users/users';
 function App() {
         const [data, setData]: any = useState([]);
 
-        const [token, setToken] = useState();
+        const [token, setToken] = useState<string>();
+
+        /*        const headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        headers.append('Authorization', `Bearer ${token?.access_token}`); */
+
+        //const options = { method: 'GET', headers: headers };
+
         useEffect(() => {
-                fetch(baseUrl)
+                fetch(baseUrl, {
+                        headers: {
+                                Content_Type: 'application/json',
+                                Authorization: `Bearer ${token}`,
+                        },
+                })
                         .then((response) => response.json())
                         .then((donnee) => setData(donnee))
                         .catch((erreur) => `${erreur}`);
         }, []);
 
-        console.log(token);
+        console.log('token', token);
 
-        const affichage = data.map((data: User) => (
-                <div>
-                        nom : {data.nom}
-                        <br />
-                        prénom : {data.prenom}
-                        <br />
-                        pseudo : {data.pseudo}
-                        <br />
-                        adresse : {data.adresse}
-                        <br />
-                        code postal : {data.codepostal}
-                        <br />
-                        ville : {data.ville}
-                        <br />
-                        departement : {data.departement}
-                        <br />
-                        <br />
-                        <br />
-                </div>
-        ));
+        console.log('data', data);
+
+        let affichage;
+
+        if (data[0]) {
+                affichage = data.map((data: User) => (
+                        <div>
+                                nom : {data.nom}
+                                <br />
+                                prénom : {data.prenom}
+                                <br />
+                                pseudo : {data.pseudo}
+                                <br />
+                                adresse : {data.adresse}
+                                <br />
+                                code postal : {data.codepostal}
+                                <br />
+                                ville : {data.ville}
+                                <br />
+                                departement : {data.departement}
+                                <br />
+                                <br />
+                                <br />
+                        </div>
+                ));
+        }
 
         return (
                 <div>
