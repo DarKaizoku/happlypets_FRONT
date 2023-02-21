7c0c6ca7a805e3ef4b9f9bf9fc90ac5b88638ce4
GARITEY Jean Sebastien (SNCF RESEAU / INFRAPOLE AQUITAINE / UP4 CSA) <7604674a@commun.ad.sncf.fr>
2023-02-21 10:21:51 +0100

    creation navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,10 @@
+import * as Bootstrap from 'bootstrap';
+export default function navbar() {
+    return (
+        <nav className="navbar navbar-primary bg-primary">
+            <a className="navbar-brand" href="#">
+                Laissez votre animal avoir un HAPPLY day avec HAPPLY PETS
+            </a>
+        </nav>
+    );
+}
