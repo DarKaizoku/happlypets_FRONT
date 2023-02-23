c95ba6a222a5e5bee3c939da686b18715e69ac83
GARITEY Jean Sebastien (SNCF RESEAU / INFRAPOLE AQUITAINE / UP4 CSA) <7604674a@commun.ad.sncf.fr>
2023-02-23 09:04:37 +0100

    feat:liaison des components avec les item de la navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,10 +1,23 @@
 import { useState } from 'react';
+import CarnetId from '../formulaire_animal/carnet_sante';
+import { FormulaireAnimal } from '../formulaire_animal/formulaire';
+import { InputAnimal } from '../formulaire_animal/input_animal';
+import { FormulaireUser } from '../formulaire_user/formulaire_user';
 
 import Login from '../Login';
 import './navbar.css';
 export default function Navbar({ setToken }: any) {
+    const [fiche, setFiche] = useState('app');
+
     return (
         <div className="container-fluid couleur shadow rounded-bottom mb-5">
+            <div className="text-end">
+                <Login
+                    className="nav-link  text-light"
+                    href="#"
+                    setToken={setToken}
+                />
+            </div>
             <div className="text-center">
                 <img src="logo.png" style={{ height: 120 }} alt="logo" />
             </div>
@@ -31,11 +44,12 @@ export default function Navbar({ setToken }: any) {
                     >
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <Login
-                                    className="nav-link  text-light"
-                                    href="#"
-                                    setToken={setToken}
-                                />
+                                <a
+                                    className="nav-link  text-light couleur "
+                                    onClick={() => setFiche('formulaire_user')}
+                                >
+                                    Inscription
+                                </a>
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link  text-light" href="#">
@@ -54,7 +68,22 @@ export default function Navbar({ setToken }: any) {
                                 </a>
                                 <ul className="dropdown-menu bg-warning">
                                     <li>
-                                        <a className="dropdown-item " href="#">
+                                        <a
+                                            className="dropdown-item"
+                                            href="#"
+                                            onClick={() => setFiche('animal')}
+                                        >
+                                            Ajouter un animal
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            className="dropdown-item "
+                                            href="#"
+                                            onClick={() =>
+                                                setFiche('carnetDeSante')
+                                            }
+                                        >
                                             Carnet de santé
                                         </a>
                                     </li>
@@ -63,7 +92,6 @@ export default function Navbar({ setToken }: any) {
                                             Agenda
                                         </a>
                                     </li>
-
                                     <li>
                                         <a className="dropdown-item" href="#">
                                             Bien-être
@@ -89,6 +117,9 @@ export default function Navbar({ setToken }: any) {
                     </div>
                 </div>
             </nav>
+            {fiche === 'carnetDeSante' && <CarnetId />}
+            {fiche === 'formulaire_user' && <FormulaireUser />}
+            {fiche === 'animal' && <FormulaireAnimal />}
         </div>
     );
 }
